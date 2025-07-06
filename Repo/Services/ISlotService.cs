305dd7193b4c8f82ce8b4e381b6e1fb29eafd556
Lekhya989 <lekhyachowdary9@gmail.com>
2025-07-06 21:11:49 +0530

    Appointment Manager

---
+++ Repo/Services/ISlotService.cs
@@ -0,0 +1,17 @@
+﻿using ApptManager.Models;
+
+namespace ApptManager.Repo.Services
+{
+    namespace ApptManager.Services
+    {
+        public interface ISlotService
+        {
+            Task GenerateSlots(int taxProfessionalId, DateTime start, DateTime end);
+            Task<List<Slot>> GetSlotsByTaxPro(int taxProfessionalId);
+            Task<string> UpdateSlot(Slot slot);
+            Task<string> DeleteSlot(int id);
+
+            Task<Slot> GetSlotByIdAsync(int slotId);
+        }
+    }
+}
